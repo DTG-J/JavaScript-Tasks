a1525d1e4a3a0f63214af459c5f640a5585dbbeb
DTG-J <grozdanovdt@abv.bg>
2024-11-03 12:31:07 +0200

    Java Script Array tasks

---
+++ SumArrayElements.js
@@ -0,0 +1,31 @@
+const firstArr = [20, 30, 40]	    //60
+input = firstArr;
+//let secondArr = [10, 17, 22, 33]	//43
+//const thirdArr = [11, 58, 69]	    //80
+
+//console.log (firstArr);
+//let filteredArr = firstArr.filter (number => number > 20)
+
+//console.log (filteredArr);
+
+//function solve(input) {
+let first=input[0];
+let last=input[input.length-1]
+console.log(first+last) 
+//};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
