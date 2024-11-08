9d780da351e04df529844a5a446fbfc1d50815c9
DTG-J <grozdanovdt@abv.bg>
2024-11-08 18:45:42 +0200

    Update AddSubtract.js

--- Functions and Statements/AddSubtract.js
+++ Functions and Statements/AddSubtract.js
@@ -1,12 +1,15 @@
 
+// function addSubtract (num1, num2, num3){
+//     const sum = num1 + num2;
+//     const result = sum - num3;
+//     return result;
+// }
+
 function addSubtract (num1, num2, num3){
-    const sum = num1 + num2;
-    const result = sum - num3;
-    return result;
+    const sum = (x, y) => x + y;
+    const subtract = (x, y) => x - y;
+    return subtract (sum (num1, num2), num3);
 }
-
-
-
 
 console.log (addSubtract (23, 6, 10)  )   //19
 console.log (addSubtract (1, 17, 30)  )   //-12
