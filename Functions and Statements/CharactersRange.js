e6ae7921c5004a2603a474e33226628a4d3256d7
DTG-J <grozdanovdt@abv.bg>
2024-11-09 06:33:11 +0200

    Create CharactersRange.js

---
+++ Functions and Statements/CharactersRange.js
@@ -0,0 +1,17 @@
+function charactersInRange (char1, char2){
+        const startCode = Math.min(char1.charCodeAt(0), char2.charCodeAt(0));
+        const endCode = Math.max(char1.charCodeAt(0), char2.charCodeAt(0));
+
+        let result = '';
+      
+        for (let code = startCode + 1; code < endCode; code++) { // +1 and < to exclude boundaries
+          result+=(String.fromCharCode(code)+' ');
+        }
+        console.log (result)
+      }
+      
+console.log (charactersInRange ('a', 'd') ) //	b c
+console.log (charactersInRange ('#', ':') ) //	$ % & ' ( ) * + , - . / 0 1 2 3 4 5 6 7 8 9
+console.log (charactersInRange ('C', '#') ) //	$ % & ' ( ) * + , - . / 0 1 2 3 4 5 6 7 8 9 : ; < = > ? @ A B
+
+
