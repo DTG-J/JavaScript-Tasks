8e30b60efc30d0070e81399bbad965053f167a8b
DTG-J <grozdanovdt@abv.bg>
2024-11-10 08:59:06 +0200

    Update PalindromeIntegers.js

--- Functions and Statements/PalindromeIntegers.js
+++ Functions and Statements/PalindromeIntegers.js
@@ -1,7 +1,18 @@
-function isPalindrome(num1, num2, num3, num4){
+
+function checkForPalindrome(numbers){
+
+    const isPalindrome = (num) => {
+        const strNum = num.toString(); 
+        const strNumReverced = strNum.split('').reverse().join('');
+
+        return strNum===strNumReverced;
+
+    }
+
+    numbers.forEach(num => console.log (isPalindrome (num)));
     
 }
 
 
 
-[123,323,421,121]	//false true false true
+checkForPalindrome ([123,323,421,121])	//false true false true
