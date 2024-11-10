36d54668088e9c32af585221bdca2af106841603
DTG-J <grozdanovdt@abv.bg>
2024-11-10 07:26:41 +0200

    another way to solve this task

--- Functions and Statements/OddEvenSum.js
+++ Functions and Statements/OddEvenSum.js
@@ -1,13 +1,23 @@
-function oddEvenSum (num){
+// function oddEvenSum (num){
   
+//     const digits = Math.abs(num).toString().split('').map(Number);
+//     const evenSum = digits.filter(d => d % 2 === 0).reduce ((sum, d) => sum + d, 0);
+//     const oddSum = digits.filter(d => d % 2 !== 0).reduce ((sum, d) => sum + d, 0);
+
+// console.log(`Odd sum = ${oddSum}, Even sum = ${evenSum}`);
+// }
+
+
+function oddEvenSum (num){
+
     const digits = Math.abs(num).toString().split('').map(Number);
-    const evenSum = digits.filter(d => d % 2 === 0).reduce ((sum, d) => sum + d, 0);
-    const oddSum = digits.filter(d => d % 2 !== 0).reduce ((sum, d) => sum + d, 0);
 
+    let evenSum = 0; oddSum = 0; 
+    digits.forEach(d => (d % 2 === 0) ? evenSum += d : oddSum += d);
+  
+    
     console.log(`Odd sum = ${oddSum}, Even sum = ${evenSum}`);
-   
 }
-
     
 
 oddEvenSum (1000435);
---
+++ Functions and Statements/PalindromeIntegers.js
@@ -0,0 +1,7 @@
+function isPalindrome(num1, num2, num3, num4){
+    
+}
+
+
+
+[123,323,421,121]	//false true false true
