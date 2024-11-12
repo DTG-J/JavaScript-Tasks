51fcd09f1a175daa47812ff3aa538f38c21dea68
DTG-J <grozdanovdt@abv.bg>
2024-11-12 18:17:55 +0200

    Update ObjectDemo.js

--- Objects and classes/ObjectDemo.js
+++ Objects and classes/ObjectDemo.js
@@ -1,4 +1,22 @@
+//Object literal
 let firstCat = {name: 'Zuza', age: 8};
 console.log  (firstCat);
 
-let secondCat = {name: 'Aria', age: 5, hairAmount: 'alot' }
+//Multiline literal
+let secondCat = {
+    name: 'Aria',
+    age: 5,
+    hairAmount: 'alot' }
+console.log(secondCat); 
+
+//Empty object
+let someCat = {}
+
+//Set property dinamicaly with dot syntax 
+someCat.name = 'Bobcho';
+someCat.age = 3;
+someCat.male = true;
+console.log(someCat);
+
+//Set property dinamicaly with
+someCat['isCrazy'] = true; 
