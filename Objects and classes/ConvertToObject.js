eba1847eb26423ef180eaad72113d6e577822849
DTG-J <grozdanovdt@abv.bg>
2024-11-13 18:33:57 +0200

    Update ConvertToObject.js

--- Objects and classes/ConvertToObject.js
+++ Objects and classes/ConvertToObject.js
@@ -5,4 +5,10 @@ const jsonString = `{
 }`
 
 const jsonObject = JSON.parse (jsonString);
-console.log (jsonObject);
+//console.log (jsonObject);
+for (const key in jsonObject) {
+    console.log(`${key}: ${jsonObject[key]}`);
+  };
+//name: George
+//age: 40
+//town: Sofia
