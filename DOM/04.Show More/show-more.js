f12f5a5b0c491baa8b228a95e07b39cfb297b541
DTG-J <grozdanovdt@abv.bg>
2024-11-17 13:34:49 +0200

    Update show-more.js

--- DOM/04.Show More/show-more.js
+++ DOM/04.Show More/show-more.js
@@ -1,3 +1,6 @@
 function showText() {
-    // TODO
+   const textElement = document.querySelector('#text');
+   const linkElement = document.querySelector('#more');
+   textElement.style.display = 'inline'; 
+   linkElement.style.display = 'none'
 }
