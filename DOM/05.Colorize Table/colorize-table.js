3b24fe1f2e9b1527bb90ef5e4e0d0c06891bb889
DTG-J <grozdanovdt@abv.bg>
2024-11-17 14:50:01 +0200

    Update colorize-table.js

--- DOM/05.Colorize Table/colorize-table.js
+++ DOM/05.Colorize Table/colorize-table.js
@@ -1,3 +1,8 @@
 function colorize() {
-    //TODO
-}
+  
+
+  const rows = document.querySelectorAll('table tbody tr:nth-child(even');
+  [...rows].forEach((el, i) => el.style.background = 'teal')
+ 
+}//const rows = document.querySelectorAll('table tbody tr');
+ //[...rows].forEach((el, i) => {if(i % 2 !== 0)el.style.backgroundColor = 'teal'})
