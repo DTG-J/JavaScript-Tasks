7e146478ee90f073c647c6b5f5c628cebf3d7b33
DTG-J <grozdanovdt@abv.bg>
2024-11-17 12:58:21 +0200

    function write

--- DOM/03.Sum Numbers/sum-numbers.js
+++ DOM/03.Sum Numbers/sum-numbers.js
@@ -1,3 +1,7 @@
 function calc() {
-    // TODO
+    const firstNum = Number(document.getElementById('num1').value);
+    const secondNum = Number(document.getElementById('num2').value);
+    const result = firstNum + secondNum;
+
+    document.getElementById('sum').value = result;
 }
--- DOM/DOM demo.js
+++ DOM/DOM demo.js
@@ -3,3 +3,13 @@
 //     console.log(this === window); // true
 //     }
 //     foo();
+function calc(firstNum, secondNum) {
+    // const num1 = document.getElementById('firstNum');
+    // const num2 = document.getElementById('secondNum');
+    const result = firstNum  + secondNum; 
+    return(result);
+    
+   
+}
+
+console.log (calc(2, 4));
