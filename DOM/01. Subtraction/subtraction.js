675b37ba78cf2ce633fd6a0ed310aa4908a6de0d
DTG-J <grozdanovdt@abv.bg>
2024-11-20 19:20:32 +0200

    Update subtraction.js

--- DOM/01. Subtraction/subtraction.js
+++ DOM/01. Subtraction/subtraction.js
@@ -3,7 +3,7 @@ function subtract() {
     const num1 = document.querySelector('#firstNumber'); 
     const num2 = document.querySelector('#secondNumber'); 
     const resultEl = document.querySelector('#result');
-    const result = Number(num2.value)- Number(num1.value);
+    const result = Math.abs(Number(num2.value)- Number(num1.value));
     resultEl.textContent = result;
     
 }
