e9eb68b59c50fabf49fcee4b8f0a62e5004b2e92
DTG-J <grozdanovdt@abv.bg>
2024-11-20 19:18:49 +0200

    function add

--- DOM/01. Subtraction/subtraction.js
+++ DOM/01. Subtraction/subtraction.js
@@ -1,3 +1,9 @@
 function subtract() {
-    //TODO
+    
+    const num1 = document.querySelector('#firstNumber'); 
+    const num2 = document.querySelector('#secondNumber'); 
+    const resultEl = document.querySelector('#result');
+    const result = Number(num2.value)- Number(num1.value);
+    resultEl.textContent = result;
+    
 }
