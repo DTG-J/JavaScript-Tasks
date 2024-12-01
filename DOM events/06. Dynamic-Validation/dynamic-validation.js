78a9bc0dea83066c15f69903f9c530a66b163835
DTG-J <grozdanovdt@abv.bg>
2024-12-01 13:28:41 +0200

    Update dynamic-validation.js

--- DOM events/06. Dynamic-Validation/dynamic-validation.js
+++ DOM events/06. Dynamic-Validation/dynamic-validation.js
@@ -2,12 +2,15 @@ document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
     const emailEl = document.querySelector('#email');
-    const pattern = /[a-z]+@[a-z]+\.[a-z]+/;
+    const pattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
-  emailEl.addEventListener('change', (event) => {
-    if (!pattern.test(event.currentTarget.value)) {
-        return event.currentTarget.classList.add('error');
-    }
-    event.currentTarget.classList.remove('error'); 
-  }); 
+    emailEl.addEventListener('input', (event) => {
+        const value = event.currentTarget.value;
+
+        if (!pattern.test(value)) {
+            event.currentTarget.classList.add('error');
+        } else {
+            event.currentTarget.classList.remove('error');
+        }
+    });
 }
