4c254677894b655825e4063b76562fa75a8df43a
DTG-J <grozdanovdt@abv.bg>
2024-12-02 18:30:49 +0200

    Update locked-profile.js

--- DOM events/03. Locked-Profile/locked-profile.js
+++ DOM events/03. Locked-Profile/locked-profile.js
@@ -1,5 +1,16 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-    //TODO
+    document.querySelector('main').addEventListener('click', (e) => {
+
+        if (e.target.nodeName !== 'BUTTON') return;
+
+        const profileEl = e.target.closest('.profile'); 
+        const state = profileEl.querySelector('input:checked').getAttribute('id');
+
+        if (state.includes('Lock'))return;
+        
+        console.log(e.target);
+
+    });
 }
