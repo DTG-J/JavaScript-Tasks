9e93c28098d5a42dae64255f5d2925d3ea886c43
DTG-J <grozdanovdt@abv.bg>
2024-12-07 15:25:52 +0200

    Update app.js

--- HTTP and AJAX/01.Blog/app.js
+++ HTTP and AJAX/01.Blog/app.js
@@ -2,6 +2,17 @@ function attachEvents() {
 // Posts - http://localhost:3030/jsonstore/blog/posts
 // Comments - http://localhost:3030/jsonstore/blog/comments
 
+const baseUrl = 'http://localhost:3030/jsonstore/blog';
+
+const selectPosts = document.querySelector('#posts');
+const postTitleEl = document.querySelector('#post-title');
+const postBodyel = document.querySelector('#post-body');
+
+
+
+
+
+
 }
 
 //attachEvents();
