58ed69007572cb2f6656b815c989cbb41fad050f
DTG-J <grozdanovdt@abv.bg>
2024-12-08 13:34:02 +0200

    Update app.js

--- HTTP and AJAX/02.Messenger/app.js
+++ HTTP and AJAX/02.Messenger/app.js
@@ -1,5 +1,28 @@
 function attachEvents() {
-    // TODO:
+    const baseUrl = 'http://localhost:3030/jsonstore/messenger';
+
+    const outputEl = document.querySelector('#messages');                   //textAreaEL
+    const inputs = document.querySelectorAll('#controls input[name]');     //Both elements name and message
+    const submitBtnEl = document.querySelector('#submit');
+    const refreshBtnEl = document.querySelector('#refresh');
+
+    submitBtnEl.addEventListener('click', (e) => {
+        const [author, content] = [...inputs].map(field => field.value);
+        if (!author || !content) return; 
+
+        const body = {author, content}
+
+        fetch(baseUrl, {
+            method: 'POST', 
+            body: JSON.stringify(body)
+        })
+    })
+
+    
+ 
+    
+
+    
 }
 
 attachEvents();
