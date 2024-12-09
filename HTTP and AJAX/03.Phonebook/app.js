ed423bd5c707c597d23193404728921875546f4f
DTG-J <grozdanovdt@abv.bg>
2024-12-09 20:56:25 +0200

    Update app.js

--- HTTP and AJAX/03.Phonebook/app.js
+++ HTTP and AJAX/03.Phonebook/app.js
@@ -1,5 +1,22 @@
 function attachEvents() {
-    // TODO:
+    /* 
+    REST API Methods
+
+        loadContacts
+        createContact
+        deleteContact
+
+    DOM API Methods
+
+        createEntry
+        deleteEntry
+
+        loadEntriesHandler
+        createEntryHandler
+        deleteEntryHandler
+
+*/
+
 }
 
 attachEvents();
