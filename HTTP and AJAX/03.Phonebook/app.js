0cf82bb15fdeead24b7fe8110c166c4cfc345e62
DTG-J <grozdanovdt@abv.bg>
2024-12-11 20:59:23 +0200

    Update app.js

--- HTTP and AJAX/03.Phonebook/app.js
+++ HTTP and AJAX/03.Phonebook/app.js
@@ -24,36 +24,42 @@ function loadContacts(baseUrl, onSuccess) {
         .catch(error => console.error('Error: ', error));
 }
 
-function createContact(baseUrl, contact, onSuccess){
+function createContact(baseUrl, contact, onSuccess) {
     fetch(baseUrl, {
         method: 'POST',
         body: JSON.stringify(contact)
     })
         .then(response => response.json())
         .then(onSuccess)
-        .catch(error => console.error('Error: ', error));
+        .catch(error => console.error('Error: ', error));    
 }
 
-function deleteContact(baseUrl, contact, onSuccess){
-     fetch(baseUrl + '/' + contact.id, {
+function deleteContact(baseUrl, contact, onSuccess) {
+    fetch(baseUrl + '/' + contact._id, {
         method: 'DELETE'
-     })
+    })
         .then(response => response.json())
         .then(onSuccess)
-        .catch(error => console.error('Error: ', error));
+        .catch(error => console.error('Error: ', error));    
 }
 
 function createElement(tag, properties, container = null){
+    
     const element = document.createElement(tag);
 
     Object.keys(properties).forEach(key => {
-        if( typeof properties[key] === 'object'){
-        Object.assign(element.dataset, properties[key])
+        if ( typeof properties[key] === 'object' ) {
+            Object.assign( element.dataset, properties[key] )
         } else {
-        element[key] = properties[key]; 
+            element[key] = properties[key];
         }
     });
+
+    if ( container ) container.append( element );
+
+    return element;
 }
+
 
 function init() {
     
@@ -61,8 +67,8 @@ function init() {
 
     const buttonLoadEl = document.querySelector('#btnLoad');
     const buttonCreateEl = document.querySelector('#btnCreate');
-    const phonebookEl = document.querySelector('#phonebook');
-
+    const phonebookEl = document.querySelector('#phonebook')
+        
     function createEntry({ person, phone, _id }) {
         createElement(
             'button',
@@ -85,29 +91,40 @@ function init() {
         phonebookEl.querySelector(`li[data-_id="${contact._id}"]`).remove();
     }
 
-    loadContacts(baseUrl, (result) => {
-            console.log(result);
+    function createEntryHandler(e) {
+
+        const inputs = document.querySelectorAll('input[type="text"][id]');
+
+        const [ person, phone ] = [...inputs].map(field => field.value);
+
+        if ( ! person || ! phone ) return;
+        
+        const contact = { person, phone };
+
+        createContact(baseUrl, contact, (result) => {
+            createEntry(result);
         });
+    }
 
-}
+    function deleteEntryHandler(e) {
+        entryEl = e.target.closest('li');
+        
+        const contact = Object.assign({}, entryEl.dataset);
 
-function createEntryHandler(e) {
+        deleteContact(baseUrl, contact, (result) => {
+            deleteEntry(result);
+        });
+    }
 
-    const inputs = document.querySelectorAll('input[type="text"][id]');
-
-    const [ person, phone ] = [...inputs].map(field => field.value);
-
-    if ( ! person || ! phone ) return;
-    
-    const contact = { person, phone };
-
-    createContact(baseUrl, contact, (result) => {
-        createEntry(result);
+    loadContacts(baseUrl, (result) => {
+        Object.values(result).forEach(createEntry);
     });
+
+    buttonCreateEl.addEventListener('click', createEntryHandler);
+    
 }
 
 document.addEventListener('DOMContentLoaded', init);
-
+}
 
 attachEvents();
-}
