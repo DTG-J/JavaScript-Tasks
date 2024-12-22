c105d108bcdabb039244c601dbd0977e30759745
DTG-J <grozdanovdt@abv.bg>
2024-12-22 10:14:15 +0200

    Create Coercion.js

---
+++ JS demos/Coercion.js
@@ -0,0 +1,14 @@
+// The Number 10 is converted to
+// string '10' and then '+'
+// concatenates both strings   
+let x = 10 + '20';
+let y = '20' + 10;
+
+// The Boolean value true is converted
+// to string 'true' and then '+'
+// concatenates both the strings
+let z = true + '10';
+
+console.log(x);
+console.log(y);
+console.log(z);
