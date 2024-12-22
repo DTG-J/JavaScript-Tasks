a3b46439d5fa286b6d83aecd29e16d9d829affc0
DTG-J <grozdanovdt@abv.bg>
2024-12-22 10:58:46 +0200

    Update Coercion.js

--- JS demos/Coercion.js
+++ JS demos/Coercion.js
@@ -12,3 +12,21 @@ let z = true + '10';
 console.log(x);
 console.log(y);
 console.log(z);
+
+
+
+
+// String to Number Conversion
+// When an operation like subtraction (-), multiplication (*), division (/), or modulus (%) is performed, all the values that are not numbers are converted into the number data type, as these operations can be performed between numbers only. Some examples of this are shown below.
+// The string '5' is converted
+// to number 5 in all cases
+// implicitly
+let w1 = 10 - '5';
+let x1 = 10 * '5';
+let y1 = 10 / '5';
+let z1 = 10 % '5';
+
+console.log(w1);
+console.log(x1);
+console.log(y1);
+console.log(z1);
