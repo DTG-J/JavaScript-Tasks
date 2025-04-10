3330ef97cfa432a60a3fdf12f44a31cfe46295e4
DTG-J <grozdanovdt@abv.bg>
2025-04-10 20:41:27 +0300

    Create function.js

---
+++ JS demos/function.js
@@ -0,0 +1,16 @@
+function solve (num1, num2) {
+    console.log(num1 + num2);
+}
+
+solve (5, 6);
+
+
+function text (fullName, grade) {
+   console.log (`The name is: ${fullName}, with grade: ${grade.toFixed(2)}`)  
+   console.log('The name is: '+ fullName + ", with grade: " + grade.toFixed(2) );
+   
+}
+
+text ('Koko', 5.3545);
+
+
